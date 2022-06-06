a6eb8d486325824086406be11e923fc6b887f110
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2022-06-06 21:54:40 +0000

    Python Loops

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(100):
+	if i == 99:
+		print("{}".format(i))
+	else:
+		print("{:d}{:d}, ".format(i//10, i%10), end="")
+
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+	upp_case = ""
+	for i in range(len(str)):
+		if str[i] >= 'a' and str[i] <= 'z':
+			upp_case = upp_case + chr(ord(str[i])-32)
+		else:
+			upp_case = upp_case + str[i]
+	print("{:s}".format(upp_case))
