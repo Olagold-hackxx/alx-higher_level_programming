40f495375e2c5275659c3b8e34658edf3632514f
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2022-06-09 00:31:15 +0000

    Lists and tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,4 +7,4 @@ def print_matrix_integer(matrix=[[]]):
                 print()
             else:
                 print(end = " ")
-        print()
+        print("")
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,4 +4,5 @@ def max_integer(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] > _sum:
             _sum = my_list[i]
+    print("")
     return _sum
