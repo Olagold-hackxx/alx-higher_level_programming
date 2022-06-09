40f12cc530fbae67279f599b5741fe4a197b4d0c
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2022-06-09 10:10:40 +0000

    Tuples and lists

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    _sum = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > _sum:
-            _sum = my_list[i]
-    if my_list == []:    
+    if my_list != []:
+        _sum = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > _sum:
+                _sum = my_list[i]
+    else:
+        _sum = None
     return _sum
