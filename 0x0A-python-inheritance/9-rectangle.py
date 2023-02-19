b78ad801e932ced877b8a6b605adb28e513fd95f
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-02-19 18:01:17 +0000

    Fix errors

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,9 +14,13 @@ class Rectangle(BaseGeometry):
         self.integer_validator('width', width)
         self.integer_validator('height', height)
 
+    def area(self):
         """Implementing Public method area"""
         return self.__width * self.__height
-
+    
     def __str__(self):
         """ Public method for printed string representation. """
         return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+r = Rectangle(1411, 781)
+print(r)
