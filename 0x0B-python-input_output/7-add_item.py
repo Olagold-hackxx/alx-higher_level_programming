f7d7f5311a2dbc43c9e6989a0eb994fb6f09b15f
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-03-22 22:14:39 +0000

    Fix pep errors

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,16 +7,16 @@ import json
 from sys import argv
 from os.path import exists
 
+if __name__ == "__main__":
+    save = __import__('5-save_to_json_file').save_to_json_file
+    load = __import__('6-load_from_json_file').load_from_json_file
 
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+    if not exists("add_item.json"):
+        with open("add_item.json", mode="w", encoding="utf-8") as f:
+            f.write('[]')
 
-if not exists("add_item.json"):
-    with open("add_item.json", mode="w", encoding="utf-8") as f:
-        f.write('[]')
+    obj = load("add_item.json")
+    for arg in argv[1:]:
+        obj.append(arg)
 
-obj = load("add_item.json")
-for arg in argv[1:]:
-    obj.append(arg)
-
-save(obj, "add_item.json")
+    save(obj, "add_item.json")
