9e2b85f31801781b464cd034ca36514886f27c72
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-03-28 04:53:21 -0400

    Fix pep8 error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,12 +9,16 @@ class Base:
     """
     Class Base will be the “base” of all other classes
     in this project. The goal of it is to manage id attribute in
-    all your future classes and to avoid duplicating the same code (by extension, same bugs)
+    all your future classes and to avoid duplicating the
+    same code (by extension, same bugs)
     Private class attribute XXX
     class constructor: def __init__(self, id=None)::
-    if id is not None, assign the public instance attribute id with this argument value
-    you can assume id is an integer and you don’t need to test the type of it
-    otherwise, increment __nb_objects and assign the new value to the public instance attribute id
+    if id is not None, assign the public instance attribute
+    id with this argument value
+    you can assume id is an integer and
+    you don’t need to test the type of it
+    otherwise, increment __nb_objects and assign
+    the new value to the public instance attribute id
     """
 
     __nb_objects = 0
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,37 @@
 #!/usr/bin/python3
+"""Contain class `Rectangle` that inherits from `Base` """
 
 
 from models.base import Base
 
+
 class Rectangle(Base):
-    
+    """
+    Class `Rectangle` that inherits from `Base`:
+
+    In the file models/rectangle.py
+    Class Rectangle inherits from Base
+    Private instance attributes, each with its own public getter and setter:
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+    Class constructor: def __init__(self, width, height, x=0, y=0, id=None):
+    Call the super class with id - this super call with use the logic
+    of the __init__ of the Base class
+    Assign each argument width, height, x and y to the right attribute
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Instantiate class object.
+        Arguments
+            width: width of rectangle
+            height: height of rectangle
+            x: x axis offset
+            y: y axis offset
+            id: object id
+        """
+
         super().__init__(id)
         self.width = width
         self.height = height
@@ -14,33 +40,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Return value of width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the value for width"""
         self.__width = value
 
     @property
     def height(self):
+        """Return value of height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the value for height"""
         self.__height = value
 
     @property
     def x(self):
+        """Return value of x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Set the value for x"""
         self.__x = value
 
     @property
     def y(self):
+        """Return value of y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Set the value for y"""
         self.__y = value
-
