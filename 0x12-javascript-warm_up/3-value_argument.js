1a439c952918034718be948cbfb6f1ec1cfe2b91
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-06-03 06:47:14 -0400

    Complete manadatory tasks

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-let myStr;
 
-if (process.argv.length < 3) {
-  myStr = 'No argument';
-} else {
-  myStr = process.argv[2];
-}
-console.log(myStr);
+console.log(process.argv[2] || 'No argument');
