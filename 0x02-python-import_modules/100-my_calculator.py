f3f9e3b92d9cee2a98a462d97fb3a6d54434978f
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-06-04 07:57:43 -0400

    Advance tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,13 +11,13 @@ a = argv[1]
 b = argv[3]
 op = argv[2]
 
-if op is '+':
+if op == '+':
     print("{} + {} = {}".format(a, b, add(int(a), int(b))))
-elif op is '-':
+elif op == '-':
     print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
-elif op is '*':
+elif op == '*':
     print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
-elif op is '/':
+elif op == '/':
     print("{} / {} = {}".format(a, b, div(int(a), int(b))))
 else:
     print("{}".format("Unknown operator. Available operators: +, -, * and /"))
