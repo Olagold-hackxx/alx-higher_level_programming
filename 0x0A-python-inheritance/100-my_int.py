7b2039b16165cfcc2d4aa65a3961c054eafd5be6
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-06-04 09:04:58 -0400

    Advance tasks

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,14 +6,14 @@ class MyInt(int):
     """Redefine equality comparisons"""
     def __eq__(self, other_int):
         """Return equal if not equal"""
-        if self is not other_int:
+        if self != other_int:
             return True
         else:
             return False
 
     def __ne__(self, other_int):
         """Return not equal if equal"""
-        if self is other_int:
+        if self == other_int:
             return True
         else:
             return False
