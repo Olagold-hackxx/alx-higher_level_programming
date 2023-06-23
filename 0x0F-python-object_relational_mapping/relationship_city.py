0e523d701de84b1a27cd509482d0874feacf5b78
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2023-06-23 09:21:29 -0700

    Fix errors

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Define State model """
 from relationship_state import Base
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, ForeignKey
 
 
@@ -14,3 +13,5 @@ class City(Base):
                 primary_key=True)
 
     name = Column(String(128), nullable=False)
+
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
