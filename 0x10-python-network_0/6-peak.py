e4f6e5bffc8914cbe0097335238fde4e03037b33
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-06-30 22:32:58 -0400

    Complete tasks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,17 +9,17 @@ def find_peak(list_of_integers):
         return None
 
     length = len(list_of_integers)
-    half = length // 2
+    mid = length // 2
     if length > 1:
-        if half >= half - 1 and half >= half + 1:
-            return half
-        if (half == length - 1 or list_of_integers[half] >=
-            list_of_integers[half + 1])\
-                and (half == 0 or list_of_integers[half] >=
-                     list_of_integers[half - 1]):
-            return list_of_integers[half]
+        if mid >= mid - 1 and mid >= mid + 1:
+            return mid
+        if (mid == length - 1 or list_of_integers[mid] >=
+            list_of_integers[mid + 1])\
+                and (mid == 0 or list_of_integers[mid] >=
+                     list_of_integers[mid - 1]):
+            return list_of_integers[mid]
         if list_of_integers[0] >= list_of_integers[1]:
             return list_of_integers[0]
         if list_of_integers[-1] >= list_of_integers[-2]:
             return list_of_integers[-1]
-    return find_peak(list_of_integers[half:])
+    return find_peak(list_of_integers[mid:])
