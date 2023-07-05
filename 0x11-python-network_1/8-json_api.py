648dc6c62b7496a8a5c7be4bbede3ab2e161ce6e
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-04 22:33:39 -0400

    Worked with urllib and requests

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,7 +4,23 @@ import requests
 from sys import argv
 
 
-if type(argv[1]) is str and len(argv) >= 1:
-    q = argv[1]
-else:
-    q=""
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ''
+
+    req = requests.post(url, data={'q': q})
+
+    try:
+        req = req.json()
+
+        if not req:
+            print('No result')
+        else:
+            print('[{}] {}'.format(req.get('id'), req.get('name')))
+
+    except ValueError:
+        print('Not a valid JSON')
