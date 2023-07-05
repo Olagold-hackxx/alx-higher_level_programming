ce5a6cb07e8fd4f8e08efcc96da2f940a2e234f1
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-04 22:13:00 -0400

    Worked with urllib and requests

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,10 +6,10 @@ from sys import argv
 
 
 if __name__ == "__main__":
-req = urllib.request.Request(argv[1])
-try:
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-        print(the_page.decode('utf8'))
-except urllib.error.HTTPError as e:
-    print('Error code: {}'.format(e.code))
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            the_page = response.read()
+            print(the_page.decode('utf8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
