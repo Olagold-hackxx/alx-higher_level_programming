a3e732fa77966ede9dfcfbfc7d940895517ecd4f
Abdulqoyyum Aileru <84625469+Olagold-hackxx@users.noreply.github.com>
2023-08-03 00:11:01 +0100

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,4 @@
-const url = 'https://swapi.co/api/films/?format=json';
-$.get(url, function (data) {
+$.get("https://swapi-api.alx-tools.com/api/films/?format=json", function (data) {
   const results = data.results;
   for (let movie of results) {
 	const item = $('<li></li>').text(movie.title);
