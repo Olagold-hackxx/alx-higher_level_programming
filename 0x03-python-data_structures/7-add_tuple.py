bcc6b1b6a72d0860d61f911c336c65094371ada2
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2024-11-24 00:25:15 +0100

    Fix old codes quality issues

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,7 +6,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         for _ in range(len(list_a), 2):
             list_a.append(0)
     if len(tuple_b) < 2:
-        for i in range(len(list_b), 2):
+        for _ in range(len(list_b), 2):
             list_b.append(0)
     new_list = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
     new_tuple = tuple(new_list)
